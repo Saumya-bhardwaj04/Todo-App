06cb1f958a2418ed7889810876576c92bd3124ce
Saumya-bhardwaj04 <samisharma000@gmail.com>
2026-02-11 20:05:01 +0530

    Fix GitHub Pages base path after rename

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Todo-App/',
+  base: '/Todo-App-React/',
   plugins: [react()],
   build: {
     outDir: 'docs',
